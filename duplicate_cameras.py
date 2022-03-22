62302c102bd54fe0a38f33961435112794a46159
Adam Fischer <deen1srocks@yahoo.com>
2022-03-22 12:24:12 -0700

    Update duplicate_cameras.py

--- duplicate_cameras.py
+++ duplicate_cameras.py
@@ -1,31 +1,39 @@
-from pyfbsdk import FBSystem, FBPlayerControl
+from pyfbsdk import FBSystem, FBPlayerControl, FBMessageBox
 import copy
 
-#Create new camera for every take in the scene
+#Create new camera for all best takes in the scene
 def copy_cameras(masterCam): 
     masterCam = masterCam
     FBPlayerControl().GotoStart()
-    takeCount = FBSystem().CurrentTake.GetLayerCount()  
+    count, bestCount = 0, 0
     for take in FBSystem().Scene.Takes:
-      takeCount -= 1
-      FBSystem().CurrentTake = FBSystem().Scene.Takes[takeCount]
-      try:
-          cameraDuplicate = copy.copy(masterCam)
-      except:
-          print ("No Master Camera Found. Make sure it is named RS_VCam_Target or contains 'master'")
-          break
-      cameraDuplicate.Name = take.Name
+        FBSystem().CurrentTake = FBSystem().Scene.Takes[count]
+        takeName = FBSystem().CurrentTake.Name
+        if ("best" in takeName.lower()):
+            try:
+                cameraDuplicate = copy.copy(masterCam)
+                bestCount += 1
+            except:
+                continue
+            cameraDuplicate.Name = take.Name
+        else:
+            pass
+        count += 1
+            
+    if (bestCount < 1):
+        FBMessageBox( "Note", "No 'best' takes to duplicate camera", "OK" )
       
 #Returns master camera
 def find_master_cam():
     for camera in FBSystem().Scene.Cameras:
-        if ("VCam_Target" in camera.LongName) or ("master" in camera.LongName.lower()) :
+        if ("VCam_Target" in camera.LongName) or ("master" in camera.LongName.lower()):
             masterCam = camera
-            print (masterCam)
         else:
-            print ("No Master Camera Found. Make sure it is named RS_VCam_Target or contains 'master'")
-  
-    copy_cameras(masterCam)
+            continue
+    try:
+        copy_cameras(masterCam)
+    except:
+        print("No Master Camera Found. Make sure it is named MasterCam or contains 'master'")
 
 #entry
 if __name__ == '__builtin__':
